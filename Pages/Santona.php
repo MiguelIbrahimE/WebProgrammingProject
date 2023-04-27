3eafcc097be878fd7c3381385d90d3fa1b1969cf
MiguelIbrahimE <miguelibrahim2002@gmail.com>
2023-04-27 21:13:53 +0300

    More Info

---
+++ Pages/Santona.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/home.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script src="home.js"></script>
+    <title>Home</title>
+</head>
+<body>
+
+<?php
+
+include "../navbar/navbar.php";
+loadNavBar();
+?>
+
+
+
+
+
+<div class="slider" id="slider">
+
+
+
+
+<button class="prevButt" id="prevButt"><</button>
+<button class="nextButt" id="nextButt">></button>
+
+<div class="radioContainer">
+
+<input type="radio" class="radioButt" name="radio" id="radio1">
+<input type="radio" class="radioButt" name="radio" id="radio2">
+<input type="radio" class="radioButt" name="radio" id="radio3">
+
+
+</div>
+
+
+</div>
+
+
+
+<div class="textSlider">
+
+<marquee class = "marquee" behavior="scroll" direction="left">
+        HAPPINESS DOSEN'T HAVE JUST ONE ADDRESS
+</marquee>
+
+
+</div>
+
+
+<div class="wrapper">
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+</div>
+
+
+</body>
+</html>
