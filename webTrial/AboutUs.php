8821030f0bfb630a563f0fe74e9a1337cfcd9e4b
MiguelIbrahimE <miguelibrahim2002@gmail.com>
2023-04-27 12:47:26 +0300

    Haitham update

---
+++ webTrial/AboutUs.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="aboutUs.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    
+    <title>About Us</title>
+</head>
+<body>
+
+
+
+<nav class="mainNav">
+
+    <h1 class="mainH1">Uni Near</h1>
+
+    <ul class="mainUl">
+
+        <a href="home.php" class="aTag">
+        <li class="btn from-left">
+            Home
+            <span class="material-symbols-outlined">
+                home
+            </span>
+        </li>
+        </a>
+        <a href="#" class="aTag">
+        <li class="btn from-left">
+            Near LAU
+            <span class="material-symbols-outlined">
+                apartment
+            </span> 
+        </li>
+        </a>
+        <a href="#" class="aTag">
+        <li class="btn from-left">
+            Near AUB
+            <span class="material-symbols-outlined">
+                apartment
+            </span> 
+        </li>
+        </a>
+        <a href="contactUs.php" class="aTag">
+        <li class="btn from-left">
+            Contact Us
+            <span class="material-symbols-outlined">
+                account_circle
+            </span>
+        </li>
+        </a>
+        <a href="AboutUs.php" class="aTag">
+        <li class="btn from-left" id="aboutUs">
+            About Us
+            <span class="material-symbols-outlined">
+                help
+            </span>
+        </li>
+        </a>
+
+
+
+    </ul>
+
+
+
+</nav>
+
+
+
+
+<div class="aboutDiv">
+
+
+<h3 class="aboutH">We are a group of senior developers working our way through these difficult situations.
+We are attempting to provide LAU and AUB students with the best housing experience possible by listing all
+nearby dorms and including all necessary information, images, descriptions, and pricing lists so that all
+new and returning students have no trouble finding dorms.</h3>  
+
+
+
+</div>
+
+
+    
+</body>
+</html>
---
+++ webTrial/home.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script src="home.js"></script>
+    <title>Home</title>
+</head>
+<body>
+
+
+<nav class="mainNav">
+
+    <h1 class="mainH1">Uni Near</h1>
+
+    <ul class="mainUl">
+
+        <a href="#" class="aTag">
+        <li class="btn from-left">
+            Home
+            <span class="material-symbols-outlined">
+                home
+            </span>
+        </li>
+        </a>
+        <a href="#" class="aTag">
+        <li class="btn from-left">
+            Near LAU
+            <span class="material-symbols-outlined">
+                apartment
+            </span> 
+        </li>
+        </a>
+        <a href="#" class="aTag">
+        <li class="btn from-left">
+            Near AUB
+            <span class="material-symbols-outlined">
+                apartment
+            </span> 
+        </li>
+        </a>
+        <a href="contactUs.php" class="aTag">
+        <li class="btn from-left">
+            Contact Us
+            <span class="material-symbols-outlined">
+                account_circle
+            </span>
+        </li>
+        </a>
+        <a href="AboutUs.php" class="aTag">
+        <li class="btn from-left" id="aboutUs">
+            About Us
+            <span class="material-symbols-outlined">
+                help
+            </span>
+        </li>
+        </a>
+
+
+
+    </ul>
+
+
+
+</nav>
+
+
+
+
+
+<div class="slider" id="slider">
+
+
+
+
+<button class="prevButt" id="prevButt"><</button>
+<button class="nextButt" id="nextButt">></button>
+
+<div class="radioContainer">
+
+<input type="radio" class="radioButt" name="radio" id="radio1">
+<input type="radio" class="radioButt" name="radio" id="radio2">
+<input type="radio" class="radioButt" name="radio" id="radio3">
+
+
+</div>
+
+
+</div>
+
+
+
+<div class="textSlider">
+
+<marquee class = "marquee" behavior="scroll" direction="left">
+        HAPPINESS DOSEN'T HAVE JUST ONE ADDRESS
+</marquee>
+
+
+</div>
+
+
+<div class="wrapper">
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+    <div class="item">
+        <h2 class="overlayText">Item2</h2>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
